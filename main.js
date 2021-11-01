ff9e99ae2b53b617c32bdf7d877f685e5a0ae92d
Oleksii Biletskyi <alexbily@gmail.com>
2021-11-01 20:44:42 +0200

    added fix

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
 
-// // ========== 1 ============
-// const logger = time => console.log(`Resolved after ${time}ms`);
+// ========== 1 ============
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// const delay = ms => {
-//   // Твой код
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(ms)
-//         }, ms);
-//     })
-// };
+const delay = ms => {
+  // Твой код
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms)
+        }, ms);
+    })
+};
 
-// // Вызовы функции для проверки
+// Вызовы функции для проверки
 
 
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
 
-// // ========== 2 ============
+// ========== 2 ============
 
-// const users = [
-//   { name: 'Mango', active: true },
-//   { name: 'Poly', active: false },
-//   { name: 'Ajax', active: true },
-//   { name: 'Lux', active: false },
-// ];
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
 
-// const toggleUserState = (allUsers, userName) => {
-//   const updatedUsers = allUsers.map(user =>
-//     user.name === userName ? { ...user, active: !user.active } : user,
-//   );
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
 
-//     return new Promise(resolve => {
-//       resolve(updatedUsers)
-//   })
-// };
+    return new Promise(resolve => {
+      resolve(updatedUsers)
+  })
+};
 
-// const logger2 = updatedUsers => console.table(updatedUsers);
+const logger2 = updatedUsers => console.table(updatedUsers);
 
-// /*
-//  * Сейчас работает так
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
 //  */
-// // toggleUserState(users, 'Mango', logger);
-// // toggleUserState(users, 'Lux', logger);
-
-// /*
-//  * Должно работать так
-// //  */
-// toggleUserState(users, 'Mango').then(logger2);
-// toggleUserState(users, 'Lux').then(logger2);
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
 
 
 
